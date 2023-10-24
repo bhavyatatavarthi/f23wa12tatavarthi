6f4141e4593fc1b5cf8ca7613c913f89a922bb1e
bhavyatatavarthi <bhavyat1008@gmail.com>
2023-10-23 21:54:44 -0500

    added

---
+++ routes/computation.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  var rand =Math.random().toFixed(2);
+  var y = req.query.x;
+  if(y == undefined){
+    y =rand;
+  }
+
+    a=math.pow(rand,rand).toFixed(2);
+    b= math.copysign(rand, rand).toFixed(2);
+    c=math.tanh(rand).toFixed(2);
+    d=math.trunc(rand);
+    res.send(`Math.cos(${y,y}): ${a} Math.copysign(${y,y}) is: ${b} Math.tanh(${y}) is : ${c} math.trunc(${y})`);
+  });
+
+module.exports = router;
